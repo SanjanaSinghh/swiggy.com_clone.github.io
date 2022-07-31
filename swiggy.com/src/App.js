3ba78ecd9a4523b1553606b975e101c0c95e8af2
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-31 20:51:40 +0530

    update login

--- swiggy.com/src/App.js
+++ swiggy.com/src/App.js
@@ -1,23 +1,14 @@
 import "./App.css";
 import Navbar from "./components/lucky/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/lucky/Login";
+// import Login from "./components/lucky/Login";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        {/* <Route
-          path="/"
-          element={
-            <Private>
-              {" "}
-              <Home />
-            </Private>
-          }
-        ></Route> */}
-        <Route path="/login" element={<Login />}></Route>
+        {/* <Route path="/login" element={<Login />}></Route> */}
       </Routes>
     </div>
   );
--- swiggy.com/src/components/lucky/Navbar.jsx
+++ swiggy.com/src/components/lucky/Navbar.jsx
@@ -3,14 +3,30 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logoutsuccess } from "../../store/store/auth/action";
+import Drawer from "@mui/material/Drawer";
 
 const Navbar = () => {
   // const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [state, setState] = React.useState(false);
 
+    const toggleDrawer = (open) => {
+      setState(open);
+    };
+
+    const list = () => (
+      <Box
+        sx={{ width: 450 }}
+        role="presentation"
+        onClick={() => toggleDrawer(false)}
+        onKeyDown={() => toggleDrawer(false)}
+      >
+        data
+      </Box>
+    );
   const token = useSelector((state) => state.auth.token);
 
   const dispatch = useDispatch();
@@ -20,11 +36,19 @@ const Navbar = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
+        <Drawer
+          anchor={"right"}
+          open={state}
+          onClose={() => toggleDrawer(false)}
+        >
+          {list()}
+        </Drawer>
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             Products Pricing Blog
-            <Link to={"/"}>Private Route</Link>
+            {/* <Link to={"/"}>Private Route</Link> */}
           </Box>
+
           {token ? (
             <Box
               onClick={handlelogout}
@@ -34,11 +58,15 @@ const Navbar = () => {
               logout
             </Box>
           ) : (
-            <Link to="/login">
-              <Box style={{ cursor: "pointer" }} sx={{ flexGrow: 0 }}>
-                Login
-              </Box>
-            </Link>
+            // <Link to="/">
+            <Box
+              style={{ cursor: "pointer" }}
+              sx={{ flexGrow: 0 }}
+              onClick={() => toggleDrawer(true)}
+            >
+              Login
+            </Box>
+            // </Link>
           )}
         </Toolbar>
       </Container>
