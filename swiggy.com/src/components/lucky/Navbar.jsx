d636ec2118f17fd7eaf09dc6c9f56e577945f944
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-31 13:08:26 +0530

    Update Navbar.jsx

--- swiggy.com/src/components/lucky/Navbar.jsx
+++ swiggy.com/src/components/lucky/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { logoutsuccess } from "../store/auth/action";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  // const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-export default Navbar
+  const token = useSelector((state) => state.auth.token);
+
+  const dispatch = useDispatch();
+  const handlelogout = () => {
+    dispatch(logoutsuccess());
+  };
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            Products Pricing Blog
+            <Link to={"/"}>Private Route</Link>
+          </Box>
+          {token ? (
+            <Box
+              onClick={handlelogout}
+              style={{ cursor: "pointer" }}
+              sx={{ flexGrow: 0 }}
+            >
+              logout
+            </Box>
+          ) : (
+            <Link to="/login">
+              <Box style={{ cursor: "pointer" }} sx={{ flexGrow: 0 }}>
+                Login
+              </Box>
+            </Link>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Navbar;
