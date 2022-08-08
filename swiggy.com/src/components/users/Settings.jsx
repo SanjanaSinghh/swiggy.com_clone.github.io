58af8159487275c68447ae859710cda874cb5f8f
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-08-08 14:02:06 +0530

    update users

--- swiggy.com/src/components/users/Settings.jsx
+++ swiggy.com/src/components/users/Settings.jsx
@@ -1,9 +1,92 @@
 import React from 'react'
+import { styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
 
 const Settings = () => {
+
+  const IOSSwitch = styled((props) => (
+    <Switch
+      focusVisibleClassName=".Mui-focusVisible"
+      disableRipple
+      {...props}
+    />
+  ))(({ theme }) => ({
+    width: 50,
+    height: 26,
+    padding: 0,
+    "& .MuiSwitch-switchBase": {
+      padding: 0,
+      margin: 2,
+      transitionDuration: "300ms",
+      "&.Mui-checked": {
+        transform: "translateX(24px)",
+        color: "#fff",
+        "& + .MuiSwitch-track": {
+          backgroundColor:
+            theme.palette.mode === "light" ? "#e46d47" : "#c1d1ff",
+          opacity: 1,
+          border: 0,
+        },
+        "&.Mui-disabled + .MuiSwitch-track": {
+          opacity: 0.5,
+        },
+      },
+      "&.Mui-focusVisible .MuiSwitch-thumb": {
+        color: "#c1d1ff",
+        border: "6px solid #fff",
+      },
+      "&.Mui-disabled .MuiSwitch-thumb": {
+        color:
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[600],
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+      },
+    },
+    "& .MuiSwitch-thumb": {
+      boxSizing: "border-box",
+      width: 22,
+      height: 22,
+    },
+    "& .MuiSwitch-track": {
+      borderRadius: 26 / 2,
+      backgroundColor: theme.palette.mode === "dark" ? "#e46d47" : "#c1d1ff",
+      opacity: 1,
+      transition: theme.transitions.create(["background-color"], {
+        duration: 500,
+      }),
+    },
+  }));
+
+
   return (
-    <div>Settings</div>
-  )
+    <div className="settingsmain">
+      <div className="jse">
+        <h1 className="text9">SMS Preferences</h1>
+        <div className="jse1">
+          <div className="jse1a">
+            Order related SMS cannot be disabled as they are critical to provide
+            service
+          </div>
+          <div className="jse1b">
+            <div className="jse11">Recommendations & Reminders</div>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <div className="jse13"></div>
+            <div className="jse14">
+              Keep this on to receive offer recommendations & timely reminders
+              based on your interests
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Settings
--- swiggy.com/src/components/users/address.jsx
+++ swiggy.com/src/components/users/address.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import NoSavedAddress from "../../Images/NoSavedAddress.webp";
 
 const Address = () => {
   return (
-    <div>Address</div>
-  )
+    <div>
+      <div className="jsb">
+        <img src={NoSavedAddress} alt="" />
+        <div className="jsbp1">Can't find a door to knock</div>
+        <div className="jsbp2">You don't have an address to deliver.</div>
+      </div>
+    </div>
+  );
 }
 
 export default Address
